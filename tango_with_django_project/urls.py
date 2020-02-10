448185278d1bbfaf24640c7bf412053afa5d0692
Shayam Bhudia <2394138b@student.gla.ac.uk>
2020-02-10 20:42:52 +0000

    Chapter 4 completed
    Tests run
    return: OK

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
 	path('rango/', include('rango.urls')),
     # The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
